997f2c2ca506fb12c9bfb5c92e163c09b943146a
Dayana Cana <dcana@dc.binaryideas.local>
2016-06-21 10:20:18 +0800

    Added styles and changed content for home page

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php 
+	//use Roots\Sage\Assets;
+ ?>
+ <div class="con-header">
+ 	<div class="mebanner">
+ 		<img src="<?= get_the_post_thumbnail_url(14)?>" alt="Dayanara" class="img-responsive">
+ 		<img src="<?= the_post_thumbnail_url(14)?>" class="img-responsive">
+ 	</div>
+ </div>

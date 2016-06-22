0a55071e394d38592771909b225a86051d254f10
Dayana Cana <dcana@dc.binaryideas.local>
2016-06-22 08:33:31 +0800

    Add html code for introduction

--- front-page.php
+++ front-page.php
@@ -3,7 +3,16 @@
  ?>
  <div class="con-header">
  	<div class="mebanner">
- 		<img src="<?= get_the_post_thumbnail_url(14)?>" alt="Dayanara" class="img-responsive">
- 		<img src="<?= the_post_thumbnail_url(14)?>" class="img-responsive">
+ 		<div class="dayanlogo">
+ 			<img src="<?= wp_get_attachment_thumb_url(14)?>" alt="Dayanara" class="img-responsive pull-left">
+ 			<span class="pull-left logo-text">Dayanara<br>Caña</span>
+ 		</div> 		
+ 		<div class="banner-title">
+ 			<!-- <img src="<?= the_post_thumbnail_url()?>" class="img-responsive" style="margin: 0 auto;"> -->
+ 			D
+ 			<div class="web-intro">
+ 				<div class="introduction">Web Developer</div>
+ 			</div>
+ 		</div>
  	</div>
  </div>
